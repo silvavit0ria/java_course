7b9622ae89f14ba3428222e61f55e68e8433d751
Vitoria Fontes <113393080+silvavit0ria@users.noreply.github.com>
2023-11-29 17:51:58 -0300

    Meu primeiro Programa em java
    o programa consiste em uma validação de idade, se a idade do usuário for maior ou igual a 18 anos ele poderá continuar seu compra.

---
+++ PrimeiroProjeto01.java
@@ -0,0 +1,18 @@
+package projeto01;
+
+public class PrimeiroProjeto01 {
+	public static void main(String[] args) {
+		int idade= 14; //declare sua idade aqui
+		
+		
+	
+		if(idade>=18) { //se a idade for maior que 18 o usuario poderá continuar sua compra 
+			System.out.println("você está liberado para comprar bebida");
+		} else { // se sua idade for menor a 18 você não irá conseguir comprar a bebida
+			System.out.println("você não pode comprar essa bebida");
+		}
+		
+	}
+
+}
+
